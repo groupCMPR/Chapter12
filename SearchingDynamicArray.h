38cdada180d32b753d2630724f1320d0b34e5d8b
groupCMPR <144290496+groupCMPR@users.noreply.github.com>
2023-11-22 13:41:11 -0800

    Add files via upload

---
+++ SearchingDynamicArray.h
@@ -0,0 +1,37 @@
+//Name: Vivian Huynh
+//Date: 11/22/2023
+//Description: Header file for class SearchingDynamicArray
+
+#pragma once
+#include <iostream>  //For ostream
+#include <vector>    //For vectors
+#include <algorithm> //For sort
+#include <cstdlib>   //For srand
+#include <string>    //For to_string
+using namespace std;
+
+template <class T>
+class SearchingDynamicArray
+{
+private:
+	vector<T> myVector;
+
+public:
+	//CONSTRUCTOR
+	SearchingDynamicArray();
+
+	//ACCESSORS
+	bool isEmpty() const;
+	T getElement(char) const; //NOT DONE, binary serial searches
+
+	//MUTATORS
+	void fillArray(int);
+	void sortArray();
+	void pushElement(const T&);
+	void setClear();
+
+	//FRIEND
+	friend ostream& operator <<<>(ostream&, const SearchingDynamicArray<T>&);
+};
+
+#include "SearchingDynamicArray.template"
